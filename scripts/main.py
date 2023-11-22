93510223da9e3c4a5d338f258e5ead75e3076011
ewide93 <ewide93@gmail.com>
2023-11-22 17:19:55 +0100

    Reformatted main.py in scripts folder.

--- scripts/main.py
+++ scripts/main.py
@@ -1,60 +1,62 @@
-#===================================================================================================
+# ===================================================================================================
 # Import statements.
-#===================================================================================================
+# ===================================================================================================
 import os
 import subprocess
 import sys
 import serial.tools.list_ports
 
 
-#===================================================================================================
+# ===================================================================================================
 # Main program entry-point.
-#===================================================================================================
+# ===================================================================================================
 def main() -> None:
     # ATmega328p memory specifications.
     FLASH_SIZE_BYTES = 0x8000
-    RAM_SIZE_BYTES   = 0x800
+    RAM_SIZE_BYTES = 0x800
 
     # HWID:s for my Arduino Nano devices.
-    ARDUINO_HWID_LIST = ['AB0LQH9QA', 'AB0P71KJA']
+    ARDUINO_HWID_LIST = ["AB0LQH9QA", "AB0P71KJA"]
 
     # Get path to directory of running script.
     current_dir = os.path.dirname(__file__)
 
     # Get relative path to build directory.
-    build_dir = os.path.join(current_dir, '..', 'build')
+    build_dir = os.path.join(current_dir, "..", "build")
 
     # Search build directory for .hex & .map files.
-    result = subprocess.run(['ls', f'{build_dir}'], stdout=subprocess.PIPE, text=True)
-    files = result.stdout.split('\n')
-    map_files = [file for file in files if file[-3:] == 'map']
-    hex_files = [file for file in files if file[-3:] == 'hex']
+    result = subprocess.run(["ls", f"{build_dir}"], stdout=subprocess.PIPE, text=True)
+    files = result.stdout.split("\n")
+    map_files = [file for file in files if file[-3:] == "map"]
+    hex_files = [file for file in files if file[-3:] == "hex"]
 
     # Verify that there is only one .hex file present in directory.
     if len(hex_files) > 1:
-        print('Found too many .hex files:')
+        print("Found too many .hex files:")
         for hex_file in hex_files:
-            print(f'{hex_file}')
+            print(f"{hex_file}")
         sys.exit(-1)
     elif len(hex_files) == 0:
-        print(f'No .hex files found.')
+        print("No .hex files found.")
         sys.exit(-1)
     else:
-        print(f'Found .hex file: {hex_files[0]}')
+        print(f"Found .hex file: {hex_files[0]}")
 
     # Print size of hex file.
-    result = subprocess.run(['wc', '-c', f'{build_dir}/{hex_files[0]}'], stdout=subprocess.PIPE, text=True)
-    print(f'Size of hex file: {result.stdout.split()[0]} bytes.')
+    result = subprocess.run(
+        ["wc", "-c", f"{build_dir}/{hex_files[0]}"], stdout=subprocess.PIPE, text=True
+    )
+    print(f"Size of hex file: {result.stdout.split()[0]} bytes.")
 
     # Verify that there is only one .map file present in directory.
     if len(map_files) > 1:
-        print(f'Found too many .map files: {map_files}')
+        print(f"Found too many .map files: {map_files}")
         sys.exit(-1)
     elif len(map_files) == 0:
-        print(f'No .map files found.')
+        print("No .map files found.")
         sys.exit(-1)
     else:
-        print(f'Found .map file: {map_files[0]}')
+        print(f"Found .map file: {map_files[0]}")
 
     # Placeholder, just printing contents of map file.
     # with open(f'{build_dir}/{map_files[0]}', mode='r') as istream:
@@ -67,12 +69,12 @@ def main() -> None:
     ports = serial.tools.list_ports.comports()
     for port in sorted(ports):
         if port.hwid[-9:] in ARDUINO_HWID_LIST:
-            print(f'Arduino Nano found at {port.name}')
-    
+            print(f"Arduino Nano found at {port.name}")
+
     # Exit program gracefully.
     sys.exit(0)
 
 
 # Protection from script from being run during import.
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
